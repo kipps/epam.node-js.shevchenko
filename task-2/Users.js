921f39a3c5849d7903d8843c9e3bb92e10c43ad6
Roman Shevchenko <roman_shevchenko@epam.com>
2020-08-10 19:16:30 +0300

    Joi added

--- task-2/Users.js
+++ task-2/Users.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    id: `f${(~~(Math.random()*1e8)).toString(16)}`,
+    id: 'fdfb067',
     login: 'John',
     password: '12345',
     age: 24,
--- task-2/server.js
+++ task-2/server.js
@@ -1,8 +1,15 @@
+const Joi = require('@hapi/joi');
+const validator = require('express-joi-validation').createValidator({})
+
 const express = require("express"),
   bodyParser = require("body-parser"),
   server = express();
 
 const users = require('./Users');
+
+const querySchema = Joi.object({
+  id: Joi.string().required()
+});
 
 //body parser for parsing request body
 server.use(bodyParser.json());
@@ -12,7 +19,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 
 
 //GET all users
-server.get('/user', function (req, res) {
+server.get('/user', (req, res) => {
   res.json(users);
 });
 
@@ -22,7 +29,7 @@ server.get('/user/:id', function (req, res) {
 });
 
 //POST new user
-server.post('/user', function (req, res) {
+server.post('/user', (req, res) => {
   let result = {
     id: `f${(~~(Math.random()*1e8)).toString(16)}`,
     login: req.body.login,
@@ -34,18 +41,18 @@ server.post('/user', function (req, res) {
 });
 
 //PUT edited user in-place of item with specified id
-server.put('/user/:id', function (req, res) {
+server.put('/user/:id', (req, res) => {
   users[req.params.id] = req.body;
   res.json(req.body);
 });
 
 //DELETE user with specified id
-server.delete('/user/:id', function (req, res) {
+server.delete('/user/:id', (req, res) => {
   users[req.params.id].isDeleted = true;
   res.json(req.body);
 });
 
 //START SERVER
-server.listen(3000, function () {
+server.listen(3000, () => {
   console.log("Server running");
 });
