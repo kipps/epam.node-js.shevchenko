d3d9f170480bc21a74120aade2579d9e4a84538e
romansevcenko <webgetup@gmail.com>
2020-08-12 20:59:56 +0300

    Task 2 solution updated: lint added as well as es6

--- task-2/Users.js
+++ task-2/Users.js
@@ -1,10 +1,10 @@
 module.exports = [
   {
-    id: "f4330b91",
-    login: "Zebra",
-    password: "tPaddwefff",
+    id: 'f4330b91',
+    login: 'Zebra',
+    password: 'tPaddwefff',
     age: 45,
-    isDeleted: false
+    isDeleted: false,
   },
   {
     id: 'fdfb067',
@@ -14,31 +14,45 @@ module.exports = [
     isDeleted: false,
   },
   {
-    id: "f4336af3",
-    login: "Sergey",
-    password: "dqwdwdw12345",
+    id: 'f4336af3',
+    login: 'Sergey',
+    password: 'dqwdwdw12345',
     age: 32,
-    isDeleted: false
+    isDeleted: false,
   },
   {
-    id: "f4336ag3",
-    login: "Tolaua",
-    password: "dqwdwdw12345",
+    id: 'f4336ag3',
+    login: 'Tolaua',
+    password: 'dqwdwdw12345',
     age: 32,
-    isDeleted: false
+    isDeleted: false,
   },
   {
-    id: "f19af4be",
-    login: "Tamara",
-    password: "passewoefe4553",
+    id: 'f19af4be',
+    login: 'Tamara',
+    password: 'passewoefe4553',
     age: 19,
-    isDeleted: false
+    isDeleted: false,
   },
   {
-    id: "f36f55a0",
-    login: "Anna",
-    password: "something",
+    id: 'f36f55a0',
+    login: 'Anna',
+    password: 'something',
     age: 36,
-    isDeleted: false
+    isDeleted: false,
+  },
+  {
+    id: 'f36f55a0',
+    login: 'Anola',
+    password: 'something',
+    age: 36,
+    isDeleted: false,
+  },
+  {
+    id: 'f36f55a1',
+    login: 'Annastasia',
+    password: 'something',
+    age: 67,
+    isDeleted: false,
   },
 ];
--- task-2/server.js
+++ task-2/server.js
@@ -1,14 +1,13 @@
-const Joi = require('@hapi/joi');
-const validator = require('express-joi-validation').createValidator();
+import Joi  from '@hapi/joi';
+import express from 'express';
+import { createValidator } from 'express-joi-validation';
 
-const express = require("express"),
-  server = express();
-
+const server = express();
 const users = require('./Users');
 
+const validator = createValidator();
+
 const userValidator = validator.body(Joi.object({
-  // id: Joi.string().optional(),
-  // isDeleted: Joi.bool().optional(),
   login: Joi.string()
     .alphanum()
     .min(3)
@@ -21,17 +20,16 @@ const userValidator = validator.body(Joi.object({
     .max(130),
 }));
 
-//body parser for parsing request body
+// body parser for parsing request body
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-//temperary store for `user` in memory
+// temperary store for `user` in memory
 
-
-//GET all users
+// GET all users
 server.get('/users', (req, res) => {
   let result = users;
-  if(req.query.sortBy === 'login') {
+  if (req.query.sortBy === 'login') {
     result.sort((a, b) => {
       if (a.login > b.login) {
         return 1;
@@ -41,20 +39,25 @@ server.get('/users', (req, res) => {
       }
       return 0;
     });
+  } else if (req.query.filterBy) {
+    result = users.filter((user) => user.login.includes(req.query.filterBy));
+    if (req.query.limit) {
+      result = result.slice(0, req.query.limit);
+    }
   }
   res.json(result);
 });
 
-//GET the user with specified id
-server.get('/user/:id', function (req, res) {
-  const user = users.find(item => item.id === req.params.id);
+// GET the user with specified id
+server.get('/user/:id', (req, res) => {
+  const user = users.find((item) => item.id === req.params.id);
   res.json(user);
 });
 
-//POST new user
+// POST new user
 server.post('/user', userValidator, (req, res) => {
-  let result = {
-    id: `f${(~~(Math.random() * 1e8)).toString(16)}`,
+  const result = {
+    id: `f${(+new Date).toString(16)}`,
     login: req.body.login,
     password: req.body.password,
     age: req.body.age,
@@ -63,19 +66,19 @@ server.post('/user', userValidator, (req, res) => {
   users.push(result); res.json(result);
 });
 
-//PUT edited user in-place of item with specified id
+// PUT edited user in-place of item with specified id
 server.put('/user/:id', userValidator, (req, res) => {
   users[req.params.id] = req.body;
   res.json(req.body);
 });
 
-//DELETE user with specified id
+// DELETE user with specified id
 server.delete('/user/:id', (req, res) => {
   users[req.params.id].isDeleted = true;
   res.json(req.body);
 });
 
-//START SERVER
+// START SERVER
 server.listen(3000, () => {
-  console.log("Server running");
+  console.log('Server running');
 });
