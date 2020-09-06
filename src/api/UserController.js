92d9f8a506071d18f0b93a556c6088cbbec5846d
Roman Shevchenko <roman_shevchenko@epam.com>
2020-09-06 23:56:34 +0300

    UserController created

--- src/api/UserController.js
+++ src/api/UserController.js
@@ -1,5 +1,11 @@
+import express from 'express';
 import Joi from '@hapi/joi';
 import {createValidator} from 'express-joi-validation';
+import UserService from '../services/UserService';
+import {UserModel} from "../models/UserModel";
+
+const router = express.Router();
+const userService = new UserService(UserModel);
 
 const validator = createValidator();
 
@@ -16,38 +22,34 @@ const userValidator = validator.body(Joi.object({
         .max(130)
 }));
 
+router.get('/users', async (req, res) => {
+    let result = await userService.getUsers();
+    res.json(result);
+});
 
-const UserController = (router) => {
+router.post('/users', userValidator, async (req, res) => {
+    let result = await userService.creatUser(req.body);
+    res.json(result);
+});
 
-    router.get('/users', async (req, res) => {
-        let result = await userService.getUsers();
-        res.json(result);
-    });
+router.get('/user/:id', async (req, res) => {
+    let result = await userService.getUser(req.params.id);
+    res.json(result);
+});
 
-    router.post('/users', userValidator, async (req, res) => {
-        let result = await userService.creatUser(req.body);
-        res.json(result);
-    });
+router.put('/user/:id', userValidator, async (req, res) => {
+    let result = await userService.updateUser(req.body, req.params.id);
+    res.json(result);
+});
 
-    router.get('/user/:id', async (req, res) => {
-        let result = await userService.getUser(req.params.id);
-        res.json(result);
-    });
+router.put('/user/:id', userValidator, async (req, res) => {
+    let result = await userService.updateUser(req.body, req.params.id);
+    res.json(result);
+});
 
-    router.put('/user/:id', userValidator, async (req, res) => {
-        let result = await userService.updateUser(req.body, req.params.id);
-        res.json(result);
-    });
+router.delete('/user/:id', async (req, res) => {
+    let result = await userService.deleteUser(req.params.id);
+    res.json(result);
+});
 
-    router.put('/user/:id', userValidator, async (req, res) => {
-        let result = await userService.updateUser(req.body, req.params.id);
-        res.json(result);
-    });
-
-    router.delete('/user/:id', async (req, res) => {
-        let result = await userService.deleteUser(req.params.id);
-        res.json(result);
-    });
-}
-
-export default UserController;
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,35 +1,13 @@
 import express from 'express';
-import {Sequelize, DataTypes, QueryTypes, Model} from 'sequelize';
-import {initUserModel, UserModel} from './models/UserModel';
-import UserService from './services/UserService';
-import UserController from "./api/UserController";
-
-import Joi from '@hapi/joi';
-import {createValidator} from 'express-joi-validation';
+import {Sequelize} from 'sequelize';
+import {initUserModel} from './models/UserModel';
+import router from "./api/UserController";
 
 const app = express();
-const router = express.Router();
-
-const userService = new UserService(UserModel);
-
 const sequelize = new Sequelize('postgres', 'postgres', 'kipps87', {
     dialect: "postgres"
 });
 
-const validator = createValidator();
-
-const userValidator = validator.body(Joi.object({
-    login: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
-    password: Joi.string().regex(/^[\w]{8,30}$/),
-    age: Joi.number()
-        .integer()
-        .min(4)
-        .max(130)
-}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -38,31 +16,6 @@ app.use(express.urlencoded({extended: true}));
 initUserModel(sequelize);
 
 sequelize.sync();
-
-// const userRouter = UserController(router);
-
-router
-    .get('/users', async (req, res) => {
-        let result = await userService.getUsers(req.query.login, req.query.limit);
-        res.json(result);
-    })
-    .post('/users', userValidator, async (req, res) => {
-        let result = await userService.creatUser(req.body);
-        res.json(result);
-    })
-    .get('/users/:id', async (req, res) => {
-        console.log('test');
-        let result = await userService.getUser(req.params.id);
-        res.json(result);
-    })
-    .put('/users/:id', userValidator, async (req, res) => {
-        let result = await userService.updateUser(req.body, req.params.id);
-        res.json(result);
-    })
-    .delete('/users/:id', async (req, res) => {
-        let result = await userService.deleteUser(req.params.id);
-        res.json(result);
-    });
 
 // START SERVER
 app.listen(3000, () => {
