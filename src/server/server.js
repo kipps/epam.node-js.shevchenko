949603d10767b72b52338235dd900d6ed99a6372
Kipps <webgetup@gmail.com>
2020-09-29 03:13:10 +0300

    Logging

--- src/server/server.js
+++ src/server/server.js
@@ -3,11 +3,11 @@ import { UserModel } from '../models/UserModel';
 import GroupService from '../services/GroupService';
 import { GroupModel } from '../models/GroupModel';
 import express from 'express';
-import invocationLogging from '../middlewares/invoketion-logging';
 import userRouter from '../api/UserController';
 import groupRouter from '../api/GroupController';
 import authRouter from '../api/AuthenticateController';
-import errorHandling from '../middlewares/error-handling';
+import expressWinston from 'express-winston';
+import winston from 'winston';
 
 
 const runServer = (app) => {
@@ -18,20 +18,34 @@ const runServer = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
-    app.use(
-        '/',
-        invocationLogging
-    );
-
+    app.use(expressWinston.logger({
+        transports: [
+            new winston.transports.File({ filename: 'response-errors.log' })
+        ],
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.json()
+        ),
+        meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+        msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+        expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+        colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+        ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+    }));
 
     app.use(authRouter(userService));
     app.use(userRouter(userService));
     app.use(groupRouter(groupService));
 
-
-    app.use(errorHandling);
-
-
+    app.use(expressWinston.errorLogger({
+        transports: [
+            new winston.transports.File({ filename: 'error.log', level: 'error' })
+        ],
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.json()
+        )
+    }));
 
     // START SERVER
     app.listen(3000, () => {
