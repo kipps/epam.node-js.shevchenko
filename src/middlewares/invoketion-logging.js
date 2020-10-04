c050728e40a49a84d9aef0f382320416d474b0fa
Kipps <webgetup@gmail.com>
2020-10-04 22:27:36 +0300

    Small fix of lint

--- src/middlewares/invoketion-logging.js
+++
@@ -1,8 +0,0 @@
-import logger from '../logging/logger';
-
-const invocationLogging = (err, req, res, next) => {
-    logger.info('API Method Invokation',  { method, url, body });
-    next(err);
-};
-
-export default invocationLogging;
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -41,7 +41,7 @@ export default class UserService {
             where: {
                 login
             }
-        })
+        });
     }
 
     creatUser(user) {
